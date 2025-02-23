f8894a462176b597cdcd48f5095f6247d059686a
CowEstaris <vk_vm@ukr.net>
2025-02-23 20:27:01 +0200

    Initial

---
+++ mkvtrimmer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#
+# @file mkvtrimmer.py
+# @date 23-02-2025
+# @author Maxim Kurylko <vk_vm@ukr.net>
+#
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
